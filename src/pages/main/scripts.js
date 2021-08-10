1e39e1cbfa89a5c6dab04cd1a5aea14364d4ed92
SergeyAleev <82897736+SergeyAleev@users.noreply.github.com>
2021-08-10 21:07:42 +0300

    типа финальный, но нет

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -22,3 +22,16 @@ buttonClose.addEventListener('click', function() {
     popup.classList.remove('popup-open');
    })
 
+
+
+var menu = document.querySelector('.menu')
+var menuOpen = document.querySelector('.navigation__menu_button-js')
+var menuClose = document.querySelector('.navigation__close_button-js')
+
+menuOpen.addEventListener('click', function() {
+    menu.classList.add('menu-open')
+})
+menuClose.addEventListener('click', function() {
+    menu.classList.remove('menu-open')
+}
+)
